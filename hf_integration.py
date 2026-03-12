ec844990f41b77421bc2d4475b0a4e4e7c17fa91
Oswaldo Ludwig <oswaldoludwig@gmail.com>
2026-03-12 18:49:59 +0100

    Update import path for SelectiveFineTuningOptimizer

--- hf_integration.py
+++ hf_integration.py
@@ -2,7 +2,7 @@
 from typing import Dict, List, Optional
 import torch.nn as nn
 from peft import LoraConfig, TaskType, get_peft_model
-from .selective_fine_tuning import SelectiveFineTuningOptimizer  # reuse our core!
+from kappaTune.selective_fine_tuning import SelectiveFineTuningOptimizer
 
 def get_kappa_lora_config(
     model: nn.Module,
